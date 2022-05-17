0cf8af4e11607dfa56aad4d51c8c883add211c1a
PrakashPrabhu-M <54565583+PrakashPrabhu-M@users.noreply.github.com>
2022-05-17 16:57:28 +0530

    v2

--- Array/Rotation/RotateArray.js
+++ Array/Rotation/RotateArray.js
@@ -1,22 +1,22 @@
 // https://www.geeksforgeeks.org/array-rotation/?ref=lbp
-// v1
-const arr=[1,2,3,4,5];
-const steps=6;
-const n=arr.length;
+// v2
+const arr = [1, 2, 3, 4, 5];
+const steps = 2;
+const n = arr.length;
 
-console.log(rotateArr(arr,steps,n));
+console.log(rotateArr(arr, steps, n));
 
-function rotateArr(arr,d,n){
+function rotateArr(arr, d, n) {
     d=d%n;
-    // store the first d elements in a temp array
-    const temp=arr.slice(0,d);
+  // create a temp array and store values from 0 to d
+  const temp = arr.slice(0, d);
 
-    // rearrange the array overwriting the first d elements with index+d el
-    for(let i=0;i<n-d;i++)
-        arr[i]=arr[i+d];
-    
-    // rearrange the array overwritting the last d elements with temp array
-    for(let i=n-d;i<n;i++)
-        arr[i]=temp[i-n+d]
-    return arr;
+  // rearrange the actual array such that last d values are left free
+  for (let i = 0; i < n - d; i++) arr[i] = arr[i + d];
+
+  // make the last d values as the temp array
+  for (let i = n - d; i < n; i++) arr[i] = temp[i + d - n];
+
+  return arr;
 }
+
